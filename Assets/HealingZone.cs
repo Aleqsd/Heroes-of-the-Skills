9761a0378af6f084b26c2616a4fd4e0eafbe382f
Alex <aleqsd@gmail.com>
2018-03-09 16:58:43 +0100

    HealingZone

---
+++ Assets/HealingZone.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealingZone : MonoBehaviour {
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            other.gameObject.GetComponent<Health>().GetHealed(1);
+    }
+}

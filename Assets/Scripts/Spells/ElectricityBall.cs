f568df43d89b9a041c67f882dd293cba59a6686e
Sonlight <louis.beaumont@gmail.com>
2018-04-14 19:40:40 +0200

    All mage balls

---
+++ Assets/Scripts/Spells/ElectricityBall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Heroes
+{
+    public class ElectricityBall : MonoBehaviour
+    {
+        private void Start()
+        {
+
+            // Destroy the bullet after 5 seconds
+            Destroy(gameObject, 5.0f);
+        }
+
+        void OnTriggerEnter(Collider collider)
+        {
+            if (collider.gameObject.CompareTag("Enemy"))
+            {
+                Health health = collider.gameObject.GetComponent<Health>();
+                if (health)
+                {
+                    health.TakeDamage(10);
+                }
+            }
+            Destroy(gameObject);
+        }
+    }
+}
